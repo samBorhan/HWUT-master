eadfe229d527fc2ae43617b8467e357493254ce1
Sam Borhan <sborhan@seattleu.edu>
2020-05-28 12:42:35 -0700

    Rearranged  lines with white spac on ProductModelTests.cs

--- UnitTests/Models/ProductModelTests.cs
+++ UnitTests/Models/ProductModelTests.cs
@@ -10,30 +10,40 @@ namespace UnitTests
         [TestMethod]
         public void ProductModel_Constructor_Default_Should_Pass()
         {
+
             // Arrange
               
+
             // Act
             var result = new ProductModel();
 
+
             // Assert
             Assert.IsNotNull(result);
+
         }
+
 
         [TestMethod]
         public void ProductModel_Get_Date_Default_Should_Pass()
         {
             // Arrange
 
+
             // Act
             var result = new ProductModel();
 
+
             // Assert
             Assert.AreEqual(DateTime.UtcNow.ToShortDateString(), result.Date.ToShortDateString());
+
         }
+
 
         [TestMethod]
         public void ProductModel_Get_Description_Set_Value_Should_Pass()
         {
+
             // Arrange
             var result = new ProductModel();
 
@@ -41,115 +51,159 @@ namespace UnitTests
             // Act
             result.Description = "Sam";
 
+
             // Assert
             Assert.AreEqual("Sam", result.Description);
+
         }
+
 
         [TestMethod]
         public void ProductModel_Get_Sequence_Set_Value_Should_Pass()
         {
+
             // Arrange
             var result = new ProductModel();
+
 
             // Act
             result.Sequence = "Sam";
 
+
             // Assert
             Assert.AreEqual("Sam",result.Sequence);
+
         }
+
 
         [TestMethod]
         public void ProductModel_Get_Email_Default_Should_Pass()
         {
+
             // Arrange
+
 
             // Act
             var result = new ProductModel();
 
+
             // Assert
             Assert.AreEqual("Unknown", result.Email);
+
         }
+
 
         [TestMethod]
         public void ProductModel_Get_Id_Set_Value_Should_Pass()
         {
+
             // Arrange
             var result = new ProductModel();
+
 
             // Act
             result.Id = "123";
 
+
             // Assert
             Assert.AreEqual("123", result.Id);
+
         }
+
 
         [TestMethod]
         public void ProductModel_Get_Maker_Set_Value_Should_Pass()
         {
+
             // Arrange
             var result = new ProductModel();
+
 
             // Act
             result.Maker = "Sam Borhan";
 
+
             // Assert
             Assert.AreEqual("Sam Borhan", result.Maker);
+
         }
+
 
         [TestMethod]
         public void ProductModel_Get_Image_Set_Value_Should_Pass()
         {
+
             // Arrange
             var result = new ProductModel();
+
 
             // Act
             result.Image = "Sam Picture";
 
+
             // Assert
             Assert.AreEqual("Sam Picture", result.Image);
+
         }
+
 
         [TestMethod]
         public void ProductModel_Get_Url_Set_Value_Should_Pass()
         {
+
             // Arrange
             var result = new ProductModel();
+
 
             // Act
             result.Url = "Sam.SeattleU.com";
 
+
             // Assert
             Assert.AreEqual("Sam.SeattleU.com", result.Url);
+
         }
+
 
         [TestMethod]
         public void ProductModel_Get_Title_Set_Value_Should_Pass()
         {
+
             // Arrange
             var result = new ProductModel();
+
 
             // Act
             result.Title = "Sam title";
 
+
             // Assert
             Assert.AreEqual("Sam title", result.Title);
+
         }
+
 
         [TestMethod]
         public void ProductModel_Get_Logistics_Default_Should_Pass()
         {
+
             // Arrange
+
 
             // Act
             var result = new ProductModel();
 
+
             // Assert
             Assert.AreEqual("", result.Logistics);
+
         }
+
 
         [TestMethod]
         public void ProductModel_Get_Ratings_Default_Should_Pass()
         {
+
             // Arrange
             var data = new ProductModel();
 
@@ -157,13 +211,16 @@ namespace UnitTests
             // Act
             var result = data.AverageRating();
 
+
             // Assert
             Assert.AreEqual(5, result);
+
         }
 
         [TestMethod]
         public void ProductModel_toString_Default_Should_Pass()
         {
+
             // Arrange
             var data = new ProductModel();
 
@@ -171,13 +228,16 @@ namespace UnitTests
             // Act
             var result = data.ToString();
 
+
             // Assert
             Assert.IsNotNull(result);
+
         }
 
         [TestMethod]
         public void ProductModel_AverageRatings_With_Null_Ratings_Zero_Should_Pass()
         {
+
             // Arrange
             var result = new ProductModel();
 
@@ -185,13 +245,17 @@ namespace UnitTests
             // Act
             result.Ratings = null;
 
+
             // Assert
             Assert.AreEqual(0,result.AverageRating());
+
         }
+
 
         [TestMethod]
         public void ProductModel_AverageRatings_With_Zero_Total_Ratings_Zero_Should_Pass()
         {
+
             // Arrange
             var result = new ProductModel();
 
@@ -204,26 +268,31 @@ namespace UnitTests
 
             }
 
+
             // Assert
             Assert.AreEqual(0, result.AverageRating());
+
         }
 
         [TestMethod]
         public void ProductModel_AverageRatings_With_NO_Items_Zero_Should_Pass()
         {
+
             // Arrange
             var result = new ProductModel();
 
 
-            // Act
-            
-                result.Ratings = new int[0];
+            // Act   
+            result.Ratings = new int[0];
 
            
             // Assert
             Assert.AreEqual(0, result.AverageRating());
+
         }
+
     }
+
 }
 
 
